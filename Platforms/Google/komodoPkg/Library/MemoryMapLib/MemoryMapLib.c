f934d32a6bab17b297e9b9db166eb02d5ef80dbf
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-08-02 19:52:01 -0400

    Platforms: Google: Initial support for the Pixel 9 Pro XL

---
+++ Platforms/Google/komodoPkg/Library/MemoryMapLib/MemoryMapLib.c
@@ -0,0 +1,64 @@
+#include <Library/MemoryMapLib.h>
+
+STATIC
+EFI_MEMORY_REGION_DESCRIPTOR
+gMemoryDescriptor[] = {
+  // Name, Address, Length, HobOption, ResourceType, ResourceAttribute, MemoryType, ArmAttribute
+
+  // DDR Regions
+  {"UEFI FD",            0xA0011000, 0x00201000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+  {"UEFI Stack",         0xA0212000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+  {"DXE Heap",           0x890000000ULL, 0x03C00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+  {"Display Reserved",   0xFAC00000, ALIGN_VALUE ((FixedPcdGet32 (PcdFrameBufferWidth) * FixedPcdGet32 (PcdFrameBufferHeight)) * 4, SIZE_4KB), AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+
+  // iRAM Regions
+  {"IRAM",               0x02020000, 0x00018000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   DEVICE},
+  {"NS_IRAM",            0x02038000, 0x00004000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+
+  // SRAM Regions
+  {"APM SRAM",           0x15700000, 0x00058000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   DEVICE},
+
+  // Reserved Memory Regions (Carveouts from the zumapro Device Tree reserved-memory Node)
+  {"pKVM Firmware",      0x8B000000, 0x000BC000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"ECT Binary",         0x90000000, 0x00060000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"GSA Secure",         0x90200000, 0x00400000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"GXP/TPU Firmware",   0x91300000, 0x02D00000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"AOC Firmware",       0x94000000, 0x03000000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"XHCI DMA",           0x97000000, 0x00400000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Secure DRAM",        0xB6200000, 0x09E00000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Secure Log",         0xC3000000, 0x00080000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Debug Logs",         0xD8100000, 0x05F20000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Secure Page Table",  0xE0000000, 0x03800000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"CP Reserved 1",      0xE8000000, 0x02000000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"CP Reserved",        0xEA400000, 0x00C00000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"CP MSI",             0xF6200000, 0x00001000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"ABL Reserved",       0xF8800000, 0x01000000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Ramoops / DSS Logs", 0xFD3F0000, 0x0040F000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH},
+  {"Bootloader Log",     0xFD800000, 0x00100000, AddMem, MEM_RES, SYS_MEM_CAP, BsData, WRITE_BACK},
+
+  // Register Regions
+  {"Chip Info",          0x10000000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"Watchdog Timer CL0", 0x10060000, 0x00001000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"Watchdog Timer CL1", 0x10070000, 0x00001000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"GICD",               0x10400000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"GICR",               0x10440000, 0x00100000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"PMU",                0x15460000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"SYSREG HSI0",        0x11020000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"DRM Decon",          0x19470000, 0x00004000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"UART",               0x10870000, 0x00001000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"Clock Controller",   0x11000000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"SYSREG UFS",         0x13020000, 0x00001000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"UFS HCI",            0x13200000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"UFS UniPro",         0x13280000, 0x00008000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  {"CMU TOP",            0x26040000, 0x00010000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE}
+};
+
+VOID
+GetMemoryMap (
+  OUT EFI_MEMORY_REGION_DESCRIPTOR **MemoryDescriptor,
+  OUT UINT8                         *MemoryDescriptorCount)
+{
+  // Pass Data
+  *MemoryDescriptor      = gMemoryDescriptor;
+  *MemoryDescriptorCount = ARRAY_SIZE (gMemoryDescriptor);
+}
---
+++ Platforms/Google/komodoPkg/komodo.dsc
@@ -0,0 +1,91 @@
+##
+#  Copyright (c) 2011 - 2022, ARM Limited. All rights reserved.
+#  Copyright (c) 2014, Linaro Limited. All rights reserved.
+#  Copyright (c) 2015 - 2020, Intel Corporation. All rights reserved.
+#  Copyright (c) 2018, Bingxing Wang. All rights reserved.
+#  Copyright (c) Microsoft Corporation.
+#
+#  SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+
+################################################################################
+#
+# Defines Section - statements that will be processed to create a Makefile.
+#
+################################################################################
+[Defines]
+  PLATFORM_NAME                  = komodo
+  PLATFORM_GUID                  = 9146BB63-EE16-4238-A6C6-36472B925699
+  PLATFORM_VERSION               = 0.1
+  DSC_SPECIFICATION              = 0x00010005
+  OUTPUT_DIRECTORY               = Build/komodoPkg
+  SUPPORTED_ARCHITECTURES        = AARCH64
+  BUILD_TARGETS                  = RELEASE|DEBUG
+  SKUID_IDENTIFIER               = DEFAULT
+  FLASH_DEFINITION               = komodoPkg/komodo.fdf
+  USE_CUSTOM_DISPLAY_DRIVER      = 0
+
+!include S5P9875Pkg/S5P9875Pkg.dsc.inc
+
+[PcdsFixedAtBuild]
+  #
+  # Debug Output
+  #
+  # The Default Mask leaves DEBUG_INFO off, which hides the Storage and Link
+  # Bring-Up Messages. Remove the 0x40 Bit once UFS is stable.
+  #
+!if $(TARGET) == DEBUG
+  gEfiMdePkgTokenSpaceGuid.PcdDebugPrintErrorLevel|0x8007EE4F
+!endif
+
+  #
+  # DDR Memory
+  #
+  # Base is the first DRAM Bank (2 GB at 0x80000000). It must stay at BASE_2GB:
+  # RamManagerLib derives the whole Bank Layout from it (Bank 2 at 0x880000000).
+  # Size is the total visible DRAM: 16 GB minus the ABL Carveouts at the Top of
+  # DRAM, rounded down for Safety. The Boot Loader does not provide a Samsung
+  # SoC Info Structure, so RamManagerLib reads this PCD instead.
+  #
+  gArmTokenSpaceGuid.PcdSystemMemoryBase|0x80000000
+  gArmTokenSpaceGuid.PcdSystemMemorySize|0x3E0000000
+
+  #
+  # UEFI Stack
+  #
+  gArmPlatformTokenSpaceGuid.PcdCPUCoresStackBase|0xA0212000
+  gArmPlatformTokenSpaceGuid.PcdCPUCorePrimaryStackSize|0x40000
+
+  #
+  # SMBIOS
+  #
+  gSiliciumPkgTokenSpaceGuid.PcdSmbiosSystemManufacturer|"Google"
+  gSiliciumPkgTokenSpaceGuid.PcdSmbiosSystemModel|"Pixel 9 Pro XL"
+  gSiliciumPkgTokenSpaceGuid.PcdSmbiosSystemRetailModel|"komodo"
+  gSiliciumPkgTokenSpaceGuid.PcdSmbiosSystemRetailSku|"Pixel_9_Pro_XL_komodo"
+  gSiliciumPkgTokenSpaceGuid.PcdSmbiosSystemBoardModel|"Pixel 9 Pro XL"
+
+  #
+  # Simple Frame Buffer
+  #
+  gSiliciumPkgTokenSpaceGuid.PcdFrameBufferWidth|1344
+  gSiliciumPkgTokenSpaceGuid.PcdFrameBufferHeight|2992
+  gSiliciumPkgTokenSpaceGuid.PcdFrameBufferColorDepth|32
+
+[LibraryClasses]
+  #
+  # Memory Libraries
+  #
+  MemoryMapLib|komodoPkg/Library/MemoryMapLib/MemoryMapLib.inf
+
+  #
+  # Input Libraries
+  #
+  KeypadDeviceLib|komodoPkg/Library/KeypadDeviceLib/KeypadDeviceLib.inf
+
+[Components]
+  #
+  # Input
+  #
+  SiliciumPkg/Drivers/KeypadDxe/KeypadDxe.inf
+  SiliciumPkg/Drivers/KeypadDeviceDxe/KeypadDeviceDxe.inf
